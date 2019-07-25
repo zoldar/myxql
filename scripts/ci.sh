d39f379ea3d8e9f1c09c4de7bdf473f882536329
Wojtek Mach <wojtek@wojtekmach.pl>
2019-07-25 16:53:47 +0200

    wip

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,11 +4,10 @@ set -x
 
 : "${DB:=mysql:8.0}"
 : "${MYSQL_TCP_PORT:=3306}"
-: "${MYSQL_SOCKET_DIR:=/tmp/mysqld}"
+: "${MYSQL_SOCKET_DIR:=tmp/mysqld}"
 name="myxql-ci"
 
 mkdir -p $MYSQL_SOCKET_DIR
-sudo chmod 777 $MYSQL_SOCKET_DIR
 
 docker pull $DB || true
 docker rm --force $name || true
